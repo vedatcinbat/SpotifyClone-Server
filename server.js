b0b73dbcb4a78ed7f27b51e305b52df4a985e359
vedatcinbat <73201483+vedatcinbat@users.noreply.github.com>
2023-09-10 21:15:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const DB = process.env.DATABASE.replace(
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(DB);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
